acfca5616167736d39f27a029034ad7cb14f5fdb
ManzaraPlus <162752510+ManzaraPlus@users.noreply.github.com>
2024-10-05 10:38:28 +0500

    update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,2 +1,4 @@
 document.getElementById('sd1').innerHTML = 'Tabiat';
-document.getElementById('sd2').innerHTML = '';
+document.getElementById('sd2').innerHTML = 'Bahor';
+document.getElementById('sd3').innerHTML = 'Yoz';
+document.getElementById('sd4').innerHTML = 'Tabiat';
